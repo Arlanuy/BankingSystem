ab904a2644647104d564e0687eb637e6f673af27
Arlan Uy <gnhs.com@gmail.com>
2021-08-19 03:46:00 -0500

    finish project

--- BankingSystem/src/main/java/banking/Bank.java
+++ BankingSystem/src/main/java/banking/Bank.java
@@ -9,7 +9,7 @@ import java.util.Random;
  */
 public class Bank implements BankInterface {
 	private LinkedHashMap<Long, Account> accounts;
-	private Long bank_index = 0;
+	private long bank_index = 0;
 	public Bank() {
 		// complete the function
 		accounts = new LinkedHashMap<>();
